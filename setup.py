1625e177134f56aa14258ec78df5a1c824359b20
Marius Gedminas <marius@gedmin.as>
2018-11-30 19:27:04 +0200

    Reformat setup.py with black

--- setup.py
+++ setup.py
@@ -16,64 +16,65 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames), 'rb'
-                ).read().decode('utf-8')
-
-tests_require = ['z3c.form', 'zope.testing']
-
-setup (
-    name='z3c.currency',
-    version='1.2.1.dev0',
-    author = "Stephan Richter and the Zope Community",
-    author_email = "zope3-dev@zope.org",
-    description = "A currency field and support for ``z3c.form``.",
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        'Detailed Documentation\n'
-        '**********************\n'
-        + '\n' +
-        read('src', 'z3c', 'currency', 'README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-        ),
-    license = "ZPL 2.1",
-    keywords = "zope3 currency schema field widget form",
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope :: 3'],
-    url = 'http://svn.zope.org/z3c.currency',
-    packages = find_packages('src'),
-    include_package_data = True,
-    package_dir = {'':'src'},
-    namespace_packages = ['z3c'],
-    extras_require = dict(
-        test = tests_require,
-        form = ['z3c.form'],
-        ),
-    install_requires = [
-        'setuptools',
-        'zope.i18n',
-        'zope.i18nmessageid',
-        'zope.interface',
-        'zope.schema',
-        ],
-    tests_require = tests_require,
-    zip_safe = False,
-    test_suite = 'z3c.currency.tests.test_suite',
+    return (
+        open(os.path.join(os.path.dirname(__file__), *rnames), "rb")
+        .read()
+        .decode("utf-8")
     )
+
+
+tests_require = ["z3c.form", "zope.testing"]
+
+setup(
+    name="z3c.currency",
+    version="1.2.1.dev0",
+    author="Stephan Richter and the Zope Community",
+    author_email="zope3-dev@zope.org",
+    description="A currency field and support for ``z3c.form``.",
+    long_description=(
+        read("README.rst") + "\n\n" + "Detailed Documentation\n"
+        "**********************\n"
+        + "\n"
+        + read("src", "z3c", "currency", "README.rst")
+        + "\n\n"
+        + read("CHANGES.rst")
+    ),
+    license="ZPL 2.1",
+    keywords="zope3 currency schema field widget form",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Framework :: Zope :: 3",
+    ],
+    url="http://svn.zope.org/z3c.currency",
+    packages=find_packages("src"),
+    include_package_data=True,
+    package_dir={"": "src"},
+    namespace_packages=["z3c"],
+    extras_require=dict(test=tests_require, form=["z3c.form"]),
+    install_requires=[
+        "setuptools",
+        "zope.i18n",
+        "zope.i18nmessageid",
+        "zope.interface",
+        "zope.schema",
+    ],
+    tests_require=tests_require,
+    zip_safe=False,
+    test_suite="z3c.currency.tests.test_suite",
+)
