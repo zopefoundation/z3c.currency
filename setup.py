0aef20e1134d0fabe13be1ebbbe3c3118cbcf85e
Michael Howitz <icemac@gmx.net>
2024-06-14 08:42:28 +0200

    Update Python version support.

--- setup.py
+++ setup.py
@@ -36,16 +36,10 @@ setup(
     author="Stephan Richter and the Zope Community",
     author_email="zope-dev@zope.dev",
     description="A currency field and support for ``z3c.form``.",
-    long_description=(
-        read("README.rst")
-        + "\n\n"
-        + "Detailed Documentation\n"
-        + "**********************\n"
-        + "\n"
-        + read("src", "z3c", "currency", "README.rst")
-        + "\n\n"
-        + read("CHANGES.rst")
-    ),
+    long_description=(read("README.rst") + "\n\n" +
+                      "Detailed Documentation\n" + "**********************\n" +
+                      "\n" + read("src", "z3c", "currency", "README.rst") +
+                      "\n\n" + read("CHANGES.rst")),
     license="ZPL 2.1",
     keywords="zope3 currency schema field widget form",
     classifiers=[
@@ -55,11 +49,11 @@ setup(
         "License :: OSI Approved :: Zope Public License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Natural Language :: English",
@@ -72,10 +66,8 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     namespace_packages=["z3c"],
-    python_requires='>=3.7',
-    extras_require=dict(
-        test=tests_require,
-        form=["z3c.form"]),
+    python_requires='>=3.8',
+    extras_require=dict(test=tests_require, form=["z3c.form"]),
     install_requires=[
         "setuptools",
         "zope.i18n",
